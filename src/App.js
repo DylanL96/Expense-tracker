5cab59346f98be466bb275c54146b0b8e105d530
Dylan Law <dlaw96@hotmail.com>
2020-10-06 14:08:19 -0400

    added new functionalities

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const Display = (props) => {
+  return(
+    <div>${props.value}</div>
+  )
+}
+
+const Add = (props) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <form onSubmit={props.handleClick}>
+      <input value={props.newNote} onChange={props.handleChange}/>
+      <button type="submit">Save</button>
+    </form>
+  )
+}
+
+const App = () => {
+  const [value, setValue] = useState(0)
+  const [newNote, setNewNote] = useState('')
+  const handleClick = (event) => {
+    // console.log(event.target)
+    event.preventDefault()
+    setValue(newNote)
+  }
+
+  const handleChange = (event) => {
+    console.log(event.target.value)
+    setNewNote(event.target.value)
+  }
+
+
+  return (
+    <div>
+      <Display value={value}/>
+      <Add handleClick={handleClick} handleChange={handleChange}/>
     </div>
-  );
+    
+  )
 }
 
 export default App;
